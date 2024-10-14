8c320071f8c305f3b8e34995a45f04b89729ef5a
Sean <seanpatmeade@gmail.com>
2024-10-14 01:10:05 +0100

    Add in all fieds to detailed job application and add choices to status

--- job_application/models.py
+++ job_application/models.py
@@ -6,6 +6,23 @@ User = get_user_model()
 # Create your models here.
 
 class JobApplication(models.Model):
+    STATUS_CHOICES = [
+        ('not_applied', 'Not Applied Yet'),
+        ('applied', 'Applied'),
+        ('viewed', 'Application Viewed'),
+        ('under_review', 'Application Under Review'),
+        ('assessment_scheduled', 'Assessment/Test Scheduled'),
+        ('interview_scheduled', 'Interview Scheduled'),
+        ('interview_completed', 'Interview Completed'),
+        ('follow_up', 'Follow-Up Required'),
+        ('offer_extended', 'Offer Extended'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('offer_declined', 'Offer Declined'),
+        ('rejected', 'Rejected'),
+        ('withdrawn', 'Withdrawn'),
+        ('hired', 'Hired'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job_title = models.CharField(max_length = 30)
     job_link_description = models.URLField(max_length = 200)
@@ -20,7 +37,11 @@ class JobApplication(models.Model):
     manager_email = models.EmailField(max_length = 250)
     company_name = models.CharField(max_length= 60)
     company_website = models.URLField(max_length = 200)
-    status = models.CharField(max_length= 20)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='not_applied',
+    )
     notes = models.TextField()
     next_stage_date_time = models.DateTimeField()
     next_stage_prep = models.TextField()
