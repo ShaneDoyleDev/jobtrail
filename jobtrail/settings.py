5d714def5ed6d2eb8a2336e7ff019679c42256d5
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-15 16:21:20 +0100

    add cloudinary static file storage

--- jobtrail/settings.py
+++ jobtrail/settings.py
@@ -188,8 +188,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
-    print('called')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
