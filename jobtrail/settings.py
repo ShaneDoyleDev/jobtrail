7a1c63901aa0b7eaf7c72f0c30afe614b3feea27
Joseph Keane <josephkeane996@gmail.com>
2024-10-17 09:30:18 +0000

    fixed settings page for live

--- jobtrail/settings.py
+++ jobtrail/settings.py
@@ -32,8 +32,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = False
-print(DEBUG)
+DEBUG = os.getenv("DEBUG", False)
 ALLOWED_HOSTS = [
                     '127.0.0.1', 
                     '8000-shanedoyledev-jobtrail-c8eaclld72l.ws.codeinstitute-ide.net',
