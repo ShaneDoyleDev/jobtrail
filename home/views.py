7a8d837baca8dadb94d9931b92ea7c57dee9119a
Joseph Keane <josephkeane996@gmail.com>
2024-10-17 09:26:20 +0000

    moved error pages

--- home/views.py
+++ home/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'staticpages/index.html')
-
-def test_404(request):
-    return render(request, 'staticpages/404.html')
-
-def test_500(request):
-    return render(request, 'staticpages/500.html')
--- jobtrail/settings.py
+++ jobtrail/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
-
+print(DEBUG)
 ALLOWED_HOSTS = [
                     '127.0.0.1', 
                     '8000-shanedoyledev-jobtrail-c8eaclld72l.ws.codeinstitute-ide.net',
