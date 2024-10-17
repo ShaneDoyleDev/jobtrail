eec91cf2b055e7fbe457fa0aaaa32dc6418db325
Joseph Keane <josephkeane996@gmail.com>
2024-10-17 06:47:23 +0000

    404 and 500 error pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('test-404/', views.test_404, name='404_error'),
+    path('404/', views.test_404, name='404_error'),
+    path('500/', views.test_500, name='500_error'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,4 +4,7 @@ def home(request):
     return render(request, 'staticpages/index.html')
 
 def test_404(request):
-    return render(request, 'staticpages/test-404.html')
+    return render(request, 'staticpages/404.html')
+
+def test_500(request):
+    return render(request, 'staticpages/500.html')
